4de8117ca0493697a050e8f214f5b3e1867fbca8
lenovo <tarikul.ewu@gmail.com>
2023-05-09 12:59:19 +0600

    tarikulnayem94

---
+++ main.py
@@ -0,0 +1,24 @@
+# Set the value of a new variable to 3
+my_var = 3
+# Print the value assigned to my_var
+print(my_var)
+# Change the value of the variable to 100
+my_var = 100
+# Print the new value assigned to my_var
+print(my_var)
+# Increase the value by 3
+my_var = my_var + 3
+# Print the value assigned to my_var
+print(my_var)
+
+
+# Create variables
+num_years = 4
+days_per_year = 365
+hours_per_day = 24
+mins_per_hour = 60
+secs_per_min = 60
+
+# Calculate number of seconds in four years
+total_secs = secs_per_min * mins_per_hour * hours_per_day * days_per_year * num_years
+print(total_secs)
